c02d085cd01d484f21cf210f085ff4f2f9dbfe39
FrejMellberg <frej.mellberg@gmail.com>
2022-09-27 19:47:04 +0200

    uppdaterad, lite snyggare

--- quiz_med_funk.py
+++ quiz_med_funk.py
@@ -1,7 +1,7 @@
 # Ok den första blev inget bra
 # Testar att göra med dict istället
 
-# Själva quizet, en dict med index. DVS siffrorna innan
+# Själva quizet, en dict med index. DVS index = siffrorna innan
 quiz = {
     1 :{
         "fråga" : "Vilket lag är bäst i Stockholm?",
@@ -17,14 +17,14 @@ quiz = {
     }
 }
 
-# eN POÄNG räkning. måste kanske ligga i loop?
+# POÄNG räkning. måste kanske ligga i loop?
 score = 0
 # score += 1 , efter rätt svar
 
 # Det här printar nr 1 och nr 1s fråga
 # print(quiz[1]['fråga'])
 
-# Går igenom 1-3 index i dict, "frågor" är bara nr egentligen
+# Går igenom 1-3 index i dict, "frågor" är bara nummerna i dicten
 # for frågor in quiz:
        # print(quiz[frågor]['fråga'])
 
@@ -32,13 +32,13 @@ def quizinator(arg):
     global score # Hade bara kunnat skriva score = 0 här, men ville testa global
     for frågor in arg:
         print(quiz[frågor]['fråga']+"\n"+"*"*20)
-        svaret = input("Svar : ")
+        svaret = input("\nSvar : ")
         if svaret == quiz[frågor]['svar']:
             print("Rätt Svar!\n\n"+"*"*20)
             score +=1 # Varje rätt svar ger +1 till score variabeln
         else:
             print("FEL!\n\n"+"*"*20)
-    print("=*"*10+f"\nDu fick {score}/3 rätt!\n"+"=*"*10) # Printar score, och krimskrams
+    print("\n"+"=*"*10+f"\nDu fick {score}/3 rätt!\n"+"=*"*10) # Printar score, och krimskrams
 
 # Kallar på funktionen, med argumentet quiz (dvs vår dictionary)
 quizinator(quiz)
